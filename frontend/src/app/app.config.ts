a61773eddd5a2dbbf0bbff369b6aa25d0a5626b4
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-10-24 18:39:20 -0700

    Nuevo servicio para utilizar notificaciones

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,11 +2,22 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideToastr({
+      timeOut: 3000, // 3 segundos
+      positionClass: 'toast-top-right',
+      closeButton: true, // Botón de cerrar
+      preventDuplicates: true,
+      tapToDismiss: true,
+      newestOnTop: true
+    }),
+    provideAnimations()
   ]
 };
