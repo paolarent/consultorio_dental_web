565d381105715665c98fdb8d5af22e52e1397e4f
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-10-25 16:12:49 -0700

    Cambios esteticos menores

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:4200', // o tu frontend
+    origin: 'http://localhost:4200', //ruta del front
     credentials: true,
   });
 
