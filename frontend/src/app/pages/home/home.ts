064a94e76cde0de555b0fffe1e86bdb9892e6bbe
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-10-25 01:30:27 -0700

    Estructura del componente de la landing page

---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../../components/navbar/navbar";
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
