8c836f25c9213ebbf9fdec5dc6c9c3adf3c028b8
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-10-24 18:39:56 -0700

    Nuevo componente para la logica de recuperar contraseña

---
+++ frontend/src/app/components/form-restore-passw/form-restore-passw.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-restore-passw',
+  imports: [],
+  templateUrl: './form-restore-passw.html',
+  styleUrl: './form-restore-passw.css'
+})
+export class FormRestorePassw {
+
+}
