8796bed1b4c821172b58aa5561dc3c2b7fd3d034
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-11-05 23:11:49 -0700

    Mejoras a la navbar y mini menu desplegable

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -13,10 +13,25 @@ export class Navbar {
   auth = inject(AuthService);
   private router = inject(Router);
 
-  logout() {
+  menuAbierto = signal(false);  //estado del menu reactivo
+
+  menuDesplegable() {
+    this.menuAbierto.update(v => !v);
+  }
+
+  verPerfil() {
+    //redirigir al perfil del usuario
+    this.router.navigate(['']);
+    this.menuAbierto.set(false);
+  }
+
+  cerrarSesion() {
     this.auth.logout().subscribe({
-      next: () => this.router.navigate(['/login/paciente'], { replaceUrl: true }),
-      error: (err) => console.error('Error al cerrar sesión', err)
+      next: () => {
+        this.menuAbierto.set(false);
+        this.router.navigate(['/login/paciente'], {replaceUrl: true});
+      },
+        error: (err) => console.error('Error al cerrar sesion', err)
     });
   }
 }
