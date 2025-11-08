cfad5a6c0e777c5e0c611a797ba1e660929bfd83
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-11-07 18:23:36 -0700

    cambios para renderizar correctamente

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
 import { LandingPage } from '../../components/landing-page/landing-page';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Footer, Navbar, LandingPage],
+  imports: [Footer, Navbar, RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
