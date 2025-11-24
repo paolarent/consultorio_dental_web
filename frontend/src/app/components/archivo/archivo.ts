727ec03f5cad222b4fcd693612e2db158fcffec0
Paola Renteria <183154900+paolarent@users.noreply.github.com>
2025-11-23 20:52:29 -0700

    Inicio de la seccion de archivo

---
+++ frontend/src/app/components/archivo/archivo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archivo',
+  imports: [],
+  templateUrl: './archivo.html',
+  styleUrl: './archivo.css'
+})
+export class Archivo {
+
+}
